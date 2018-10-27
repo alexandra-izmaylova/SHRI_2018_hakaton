6d257ddae1447b6e57f16c167f76549834d407ee
Александра <>
2018-10-27 17:04:22 +0500

    task-2_v2

--- task_2.js
+++ task_2.js
@@ -1,15 +1,18 @@
-let arr = [-7, 1, 1, 6, -4, 0];
+let arr = [-7, 1, 1, 6, -4, 0, 2];
 let min;
+let minIndex;
 
 for(let i = 0; i < arr.length; i++) {
+    min = arr[i];
+    minIndex = i;
     for(let j = i+1; j < arr.length; j++) {
-        min = arr[i];
-        if(arr[j] <= min) {
+        if(arr[j] < min) {
             min = arr[j];
-            arr[j] = arr[i];
-            arr[i] = min;
+            minIndex = j;
         }
     }
+    arr[minIndex] = arr[i];
+    arr[i] = min;
 }
 
 console.log(arr);
